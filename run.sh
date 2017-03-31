a94f134e60501e03b8008d23efc02123af9d7576
KaneHui <18392888381@163.com>
2017-03-31 13:46:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 echo " === run test on simulator"
-echo "xcodebuild test -project '$xcodeproj' -scheme '$FLOW_IOS_COMPILE_SCHEME' -destination 'platform=iOS Simulator,OS=10.1,name=iPhone 6' -configuration Debug | xcpretty"
-xcodebuild test -project '$xcodeproj' -scheme '$FLOW_IOS_COMPILE_SCHEME' -destination 'platform=iOS Simulator,OS=10.1,name=iPhone 6' -configuration Debug | xcpretty
+echo "xcodebuild test -project '$FLOW_IOS_COMPILE_PROJECT' -scheme '$FLOW_IOS_COMPILE_SCHEME' -destination 'platform=iOS Simulator,OS=10.1,name=iPhone 6' -configuration Debug | xcpretty"
+xcodebuild test -project $FLOW_IOS_COMPILE_PROJECT -scheme $FLOW_IOS_COMPILE_SCHEME -destination 'platform=iOS Simulator,OS=10.1,name=iPhone 6' -configuration Debug | xcpretty

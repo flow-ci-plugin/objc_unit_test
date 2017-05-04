7f2d198ca8fb817ab57cd70bbd8a65149f28d92b
yang.guo <gy@fir.im>
2017-05-04 17:37:36 +0800

    add output for inputs

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "==== start to run test by fastlane ===="
 
-FLOW_OBJC_TEST_DEVICE="iPhone 6"
+echo "selected test device: $FLOW_IOS_TEST_DEVICE"
 
-fastlane scan $FLOW_FASTLANE_PARAM --device $FLOW_OBJC_TEST_DEVICE --clean
+fastlane scan $FLOW_FASTLANE_PARAM --device $FLOW_IOS_TEST_DEVICE --clean

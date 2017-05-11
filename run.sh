fe3a1df4e2775208a1e475bade93615bf6ef5cb0
Yang Guo <gy@fir.im>
2017-05-11 19:43:02 +0800

    support scheme and configuration for test phase

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 echo "==== This step must added after objc build ===="
 
+if [ -n "$FLOW_IOS_TEST_SCHEME" ]; then
+	FLOW_IOS_FASTLANE_PARAMS="--scheme $FLOW_IOS_TEST_SCHEME"
+else
+	FLOW_IOS_FASTLANE_PARAMS="--scheme $FLOW_IOS_COMPILE_SCHEME"
+fi
+
+if [ -n "$FLOW_IOS_TEST_CONFIGURATION" ]; then
+	FLOW_IOS_FASTLANE_PARAMS="$FLOW_IOS_FASTLANE_PARAMS --configuration $FLOW_IOS_TEST_CONFIGURATION"	
+else
+	FLOW_IOS_FASTLANE_PARAMS="$FLOW_IOS_FASTLANE_PARAMS --configuration $FLOW_IOS_TEST_CONFIGURATION"
+fi
+
 echo "selected test device: $FLOW_IOS_TEST_DEVICE"
 
 fastlane scan $FLOW_IOS_FASTLANE_PARAMS --device $FLOW_IOS_TEST_DEVICE --clean

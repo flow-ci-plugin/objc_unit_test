ea31d3bcad5455bf8be43a15c67ac778f5fed709
JiCheng <atpking@gmail.com>
2017-07-07 13:59:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,3 +15,6 @@ fi
 echo "selected test device: $FLOW_IOS_TEST_DEVICE"
 
 fastlane scan $FLOW_IOS_FASTLANE_PARAMS --device $FLOW_IOS_TEST_DEVICE --clean
+
+gem install flow_project_manager
+flow_project_manager test_output/report.junit
